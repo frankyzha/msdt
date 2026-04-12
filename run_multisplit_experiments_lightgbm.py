c57722e45f920b36633662eaab203f6e4a424080
Frank Zhang <frank.zhang@duke.edu>
2026-04-12 12:33:44 -0400

    Remove retired experiment debris and legacy scripts

--- run_multisplit_experiments_lightgbm.py
+++
@@ -1,7 +0,0 @@
-"""Run LightGBM-binned multi-split SPLIT experiments with VCM-friendly artifacts."""
-
-from multisplit_experiment_runner import run_main
-
-
-if __name__ == "__main__":
-    run_main("lightgbm")
